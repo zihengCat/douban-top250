631d989127061f7200c5fb4f140f349b02440520
ziheng <zihenglv@gmail.com>
2017-10-29 08:34:17 +0800

    update data structure

--- top250.py
+++ top250.py
@@ -5,33 +5,33 @@ def open_url(url):
     res = requests.get(url)
     return res
 
-def find_next(html_data):
-    soup = bs4.BeautifulSoup(html_data, "html.parser")
-    target = soup.find_all("span", class_ = "next")
-
-    print(target[0].a['href'])
-    #return target.a.text
-
 def find_movies(html_data):
     soup = bs4.BeautifulSoup(html_data, "html.parser")
-    target = soup.find_all("div", class_ = "hd")
+    target = soup.find_all("div", class_ = "item")
 
-    name = list()
+    d = dict()
     for i in target:
-        name.append(i.a.span.text)
-    return name
+        name = i.find_all("span", class_="title")[0].text
+        rank = i.find_all("div",  class_="pic")[0].em.text
+        d[rank] = name
+    return d
 
 def main():
 
     url = "https://movie.douban.com/top250"
-    l = list()
+    movie_dict = dict()
     for i in range(0, 250, 25):
         append = "?start=%d&filter=" % i
         new_url = url + append
         # print(new_url)
         res = open_url(new_url)
-        l.extend(find_movies(res.text))
-    print(l)
+        movie_dict.update(find_movies(res.text))
+    # print(movie_dict)
+    f = open("douban_top250.txt", "w", encoding= "utf-8")
+    for (k, v) in movie_dict.items():
+        f.write("%s %s\n" % (k, v))
+    f.close()
+
 
 if __name__ == "__main__":
     main()
