2654e3f565bafe807d05d8b0c23ed05d5fc1cde4
ziheng <zihenglv@gmail.com>
2017-11-10 18:01:25 +0800

    add time.sleep() function

--- douban_top250.py
+++ douban_top250.py
@@ -1,10 +1,14 @@
 import requests
 import bs4
+import time
 
+# Open URL, return Request Object
 def open_url(url):
-    res = requests.get(url)
+    ua_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"}
+    res = requests.get(url, headers = ua_headers)
     return res
 
+# Get the next URL from target HTML
 def get_next(html_data):
     soup = bs4.BeautifulSoup(html_data, "html.parser")
     target = soup.find_all("span", class_="next")
@@ -13,6 +17,7 @@ def get_next(html_data):
     else:
         return None
 
+# Find infomations, return a dict
 def find_movies(html_data):
     soup = bs4.BeautifulSoup(html_data, "html.parser")
     target = soup.find_all("div", class_ = "item")
@@ -23,7 +28,6 @@ def find_movies(html_data):
         m_name = i.find_all("span", class_="title")[0].text
         m_rating = i.find_all("span", class_="rating_num")[0].text
         # m_quote = i.find_all("span", class_="inq")[0].text
-
         d[m_id] = [m_name, m_rating]
     return d
 
@@ -33,6 +37,7 @@ def main():
     url = "https://movie.douban.com/top250"
     append = ""
     while True:
+        time.sleep(1)
         res = open_url(url + append)
         append = get_next(res.text)
         if append == None:
@@ -41,6 +46,7 @@ def main():
             print(url + append)
         movie_dict.update(find_movies(res.text))
 
+    # Save to .txt File
     print(movie_dict)
     f = open("douban_top250.txt", "w", encoding= "utf-8")
     f.write("%s, %s, %s, %s\n" % ("ID", "Name", "Rating", "Quote"))
